6e2177899653c7a42391cf99db27c00cd049421c
空晓梦 <137902205+kxmzyc@users.noreply.github.com>
2025-03-05 10:34:36 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,21 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	/* 移除所有自定义字体设置 */
+	page, .container {
+		font-family: -apple-system, BlinkMacSystemFont, 'Helvetica Neue', 'PingFang SC', 'Microsoft YaHei', sans-serif;
+	}
+</style>
